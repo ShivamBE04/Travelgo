05d64f0368e744399ada42cc92b692bdc6ede873
Shivam Bind <shivamseemabind04@gmail.com>
2025-11-24 13:23:56 +0530

    Initial MERN project

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+dotenv.config();
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// connect db
+connectDB();
+
+// test route
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
